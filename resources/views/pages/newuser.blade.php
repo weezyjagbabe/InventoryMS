fe555d0b442def1b75514ee66438a8da693b883a
Solomon Appier-Sign <braasig@gmail.com>
2017-11-21 15:12:41 +0000

    Changes made to layout

--- resources/views/pages/newuser.blade.php
+++ resources/views/pages/newuser.blade.php
@@ -1,55 +1,54 @@
 @extends ('layout.app')
-
 @section('title', 'Users')
-@section('description', 'Personnel Directory')
+@section('description', 'Create New User')
 
 @section('content')
     {{--<section class="content">--}}
         <div class="row">
             <!-- left column -->
-            <div class="box box-primary col-md-12">
+            <div class="box box-primary">
                 <!-- general form elements -->
-                <div class="row col-md-6 col-md-offset-3">
+                <div class="col-lg-8 col-lg-offset-2">
                     <p></p>
-                <div class=" box">
-                    <div class="box-header with-border">
+                <div class="box">
+                    <div class="box-header">
                         <h3 class="box-title">User Information</h3>
                     </div>
                     <!-- /.box-header -->
                     <!-- form start -->
                     <form role="form" class="">
-                        <div class="box-body  ">
-                            <div class="form-group">
+                        <div class="box-body">
+                            <div class="form-group col-md-6">
                                 <label for="first_name">First Name</label>
                                 <input type="text" class="form-control" id="first_name" placeholder="Enter FirstName">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label for="middle_name">Middle Name</label>
                                 <input type="text" class="form-control" id="middle_name" placeholder="Enter MiddleName">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label for="last_name">Last Name</label>
                                 <input type="text" class="form-control" id="last_name" placeholder="Enter LastName">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label for="exampleInputEmail1">Email address</label>
                                 <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label for="exampleInputPassword1">Password</label>
                                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label for="exampleInputPassword1">Repeat Password</label>
                                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label for="image">Image</label>
                                 <input type="file" id="image">
 
                                 <p class="help-block">Photo of user</p>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group col-md-6">
                                 <label>Department</label>
                                 <select class="form-control">
                                     <option>Accounting</option>
@@ -58,26 +57,6 @@
                                     <option>Procurement</option>
                                     <option>Dispatch</option>
                                 </select>
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> Administrator
-                                </label>
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> Accountant
-                                </label>
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> Dispatcher
-                                </label>
-                            </div>
-                            <div class="checkbox">
-                                <label>
-                                    <input type="checkbox"> Auditor
-                                </label>
                             </div>
 
                         </div>
