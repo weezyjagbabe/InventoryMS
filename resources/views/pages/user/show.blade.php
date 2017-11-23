d2bbf4d6b2dd80c2b623e647b74d2a8b4a56df00
Solomon Bortey <sbortey@payswitch.com.gh>
2017-11-23 08:01:29 +0000

    editted the pages in the user folder

--- resources/views/pages/user/show.blade.php
+++ resources/views/pages/user/show.blade.php
@@ -16,29 +16,7 @@
             </div>
             <div class="box-footer">
                 <div class="row">
-                    {{--<div class="col-sm-4 border-right">--}}
-                    {{--<div class="description-block">--}}
-                    {{--<h5 class="description-header">3,200</h5>--}}
-                    {{--<span class="description-text">SALES</span>--}}
-                    {{--</div>--}}
-                    {{--<!-- /.description-block -->--}}
-                    {{--</div>--}}
-                    {{--<!-- /.col -->--}}
-                    {{--<div class="col-sm-4 border-right">--}}
-                    {{--<div class="description-block">--}}
-                    {{--<h5 class="description-header">13,000</h5>--}}
-                    {{--<span class="description-text">FOLLOWERS</span>--}}
-                    {{--</div>--}}
-                    {{--<!-- /.description-block -->--}}
-                    {{--</div>--}}
-                    {{--<!-- /.col -->--}}
-                    {{--<div class="col-sm-4">--}}
-                    {{--<div class="description-block">--}}
-                    {{--<h5 class="description-header">35</h5>--}}
-                    {{--<span class="description-text">PRODUCTS</span>--}}
-                    {{--</div>--}}
-                    {{--<!-- /.description-block -->--}}
-                    {{--</div>--}}
+
                     <div class="col-sm-4 border-right">
                         <div class="description-block">
                             <h5 class="description-header">Alexander</h5>
